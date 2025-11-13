6fa8daad6feaddc5cf1629813162c8b5dc76e3e4
Muh-Afnan <muhammadafnan108@gmail.com>
2025-11-13 19:21:42 +0500

    Final work of the day

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def main():
     print(get_file_content("calculator", "lorem.txt"))
     print (write_file("calculator", "lorem.txt", content="This is content"))
+    print(run_python_file("calculator","main.py"))
 
 
 main()
